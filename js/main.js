6d877db4a50241eef8478e3d8203e9c74895ff9b
hanchaewoong <hco2995@naver.com>
2023-11-02 16:51:26 +0900

    nav 작업중

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ $(function(){
     });
     $(".reiew_item").on("mouseleave", function(){
         $(this).find(".reiew_text").removeClass("on")
-    })
+    });
+
+    $('.nav_bg').on("mouseover", function(){
+        $(this).addClass("on");
+        $('#gnb').find('> li').css("background", "var(--secondary-color)")
+        $('.lnb').addClass("on");
+        
+    });
 })
